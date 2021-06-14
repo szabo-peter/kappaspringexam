05688ee4b83e8764c7b23a8411f396c1a90db594
Peter Szabo <szabo.peter1991@gmail.com>
2021-06-14 09:17:22 +0200

    Initial commit

---
+++ src/main/java/hu/flowacedmy/kappaspringexam/KappaspringexamApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacedmy.kappaspringexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KappaspringexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KappaspringexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/flowacedmy/kappaspringexam/KappaspringexamApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.flowacedmy.kappaspringexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappaspringexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
